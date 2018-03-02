769bda087647ae7f9518604207adef6200428f47
DavidFok <david_fok@hotmail.com>
2018-03-02 19:34:23 +0000

    fixed bug where page would crash if /cart was accessed without being logged in | added logic to prevent stripe button from being rendered if user is not logged in

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+    http_basic_authenticate_with name: ENV['HTTP_BASE_AUTH_NAME'], password: ENV['HTTP_BASE_AUTH_PASSWORD']
+
 
   def index
     @categories = Category.order(id: :desc).all
