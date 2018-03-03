579322064ea78e5bb7cdfc07bf1cca9bf813254b
DavidFok <david_fok@hotmail.com>
2018-03-03 22:35:50 +0000

    implemented functionality to add reviews and ratings

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  helper_method :rate_calc, :blank_stars
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -6,6 +7,19 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @star_count = rate_calc(@product.reviews)
+    @blank_count = 5 - @star_count
+    @review = Review.create({product: @product})
+  end
+
+private
+
+  def rate_calc(reviews)
+    if reviews.length == 0
+      return 0
+    end
+    rating_sum = (reviews.map { |r| r.rating }).sum
+    return rating_sum / reviews.length
   end
 
 end
